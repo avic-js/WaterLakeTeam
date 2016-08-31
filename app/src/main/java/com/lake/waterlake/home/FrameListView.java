27f76d32e0ae3f33cfcb31227a82e5bf3ed79fb4
yyh <ssmmhxn@163.com>
2016-08-31 10:02:25 +0800

    23

---
+++ app/src/main/java/com/lake/waterlake/home/FrameListView.java
@@ -0,0 +1,21 @@
+package com.lake.waterlake.home;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by yyh on 16/8/31.
+ */
+public class FrameListView extends ListView {
+    public FrameListView(Context context) {
+        super(context);
+    }
+
+    public FrameListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    //
+
+    
+}
