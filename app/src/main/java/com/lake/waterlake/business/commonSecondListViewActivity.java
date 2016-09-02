657fca66d749cddfe262dd4a9927e63f52769d85
yyh <ssmmhxn@163.com>
2016-09-02 14:33:35 +0800

    yyh

--- app/src/main/java/com/lake/waterlake/business/commonSecondListViewActivity.java
+++ app/src/main/java/com/lake/waterlake/business/commonSecondListViewActivity.java
@@ -1,9 +1,26 @@
 package com.lake.waterlake.business;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.lake.waterlake.R;
+
 /**
  * Created by yyh on 16/9/2.
  *
  * 通用二级列表页面
  */
-public class commonSecondListViewActivity {
+public class commonSecondListViewActivity extends Activity{
+
+    TextView kindName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.common_sec_view);
+
+
+
+    }
 }
