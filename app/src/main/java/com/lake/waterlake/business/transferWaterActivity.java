31d1ca98387341ed6107914fc54c50dd1fd7916c
yyh <ssmmhxn@163.com>
2016-09-12 11:21:35 +0800

    yyh

--- app/src/main/java/com/lake/waterlake/business/transferWaterActivity.java
+++ app/src/main/java/com/lake/waterlake/business/transferWaterActivity.java
@@ -11,5 +11,6 @@ public class transferWaterActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    //
     }
 }
