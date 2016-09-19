4184bd18496b6931ab9f521f1a75da99a14e3704
yyh <ssmmhxn@163.com>
2016-09-19 14:04:21 +0800

    yyh

---
+++ app/src/main/java/com/lake/waterlake/business/TransferWaterActivity.java
@@ -0,0 +1,18 @@
+package com.lake.waterlake.business;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by yyh on 16/9/19.
+ */
+public class TransferWaterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(this, "drink safe", Toast.LENGTH_SHORT);
+    }
+}
