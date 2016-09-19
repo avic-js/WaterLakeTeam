722b8dbca2836189e2ec3125c0c22646989e1d82
yyh <ssmmhxn@163.com>
2016-09-19 13:45:11 +0800

    yyh

--- app/src/main/java/com/lake/waterlake/business/DrinksafeActivity.java
+++ app/src/main/java/com/lake/waterlake/business/DrinksafeActivity.java
@@ -2,6 +2,7 @@ package com.lake.waterlake.business;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Created by yyh on 16/9/12.
@@ -12,5 +13,7 @@ public class DrinksafeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Toast.makeText(this,"drink safe",Toast.LENGTH_SHORT);
     }
 }
