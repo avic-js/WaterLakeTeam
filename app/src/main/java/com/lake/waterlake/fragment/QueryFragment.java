940bf21bc457a1c91bbf2752b9e79bee3cad744c
sunnyzoush <564661958@qq.com>
2016-09-22 16:48:51 +0800

    添加网络请求的功能

--- app/src/main/java/com/lake/waterlake/fragment/QueryFragment.java
+++ app/src/main/java/com/lake/waterlake/fragment/QueryFragment.java
@@ -1,12 +1,15 @@
 package com.lake.waterlake.fragment;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.lake.waterlake.R;
 
 /**
  * Created by
@@ -14,15 +17,44 @@ import android.view.ViewGroup;
  */
 public class QueryFragment extends LazyFragment{
 
-
+    View view;
+    private Spinner spinnerDpt = null;
+    private Spinner spinnerTime = null;
+    private ArrayAdapter<CharSequence> adapterCity = null;
     @Override
     protected View initViews(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
+        view =  inflater.inflate(R.layout.query_frame,container,false);
+        //静态实现的下拉框，数据写在query_frame.xml文件中
+        spinnerDpt =(Spinner)view.findViewById(R.id.spinnerDpt);
+        spinnerDpt.setOnItemSelectedListener(new OnItemSelectedListenerImpl());
+        //动态实现的下拉框，数据在程序中获得，实际项目可能来自数据库等
+        spinnerTime = (Spinner)view.findViewById(R.id.spinnerTime);
+//        spinnerTime.setAdapter(adapterCity);
+        spinnerTime.setOnItemSelectedListener(new OnItemSelectedListenerImpl());
+        return view;
+
     }
 
     @Override
     protected void initData() {
-        System.out.print("-----QueryFragment-----");
+
     }
 
+    private class OnItemSelectedListenerImpl implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int position, long id) {
+            String time = spinnerTime.getSelectedItem().toString();
+//            parent.getItemAtPosition(position).toString();
+             String dept=spinnerDpt.getSelectedItem().toString();
+            Toast.makeText(getActivity(), "下拉框选择是：" + time+"---"+dept,
+                    Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            // TODO Auto-generated method stub
+        }
+    }
 }
+
